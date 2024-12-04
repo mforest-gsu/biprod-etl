12d8de1ade087d2f1209bddd1896d9ab08ff46cd
Melody Kimball <mforest@gsu.edu>
2024-12-04 14:42:02 -0500

    Composer updates

--- config/bundles.php
+++ config/bundles.php
@@ -1,12 +1,13 @@
 <?php
 
 return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Gadget\Cache\GadgetCacheBundle::class => ['all' => true],
+    Gadget\Console\GadgetConsoleBundle::class => ['all' => true],
     Gadget\Http\GadgetHttpBundle::class => ['all' => true],
     Gadget\Ldap\GadgetLdapBundle::class => ['all' => true],
     Gadget\Log\GadgetLogBundle::class => ['all' => true],
-    Gadget\Console\GadgetConsoleBundle::class => ['all' => true],
     Gadget\Oracle\GadgetOracleBundle::class => ['all' => true],
+    Gadget\Process\GadgetProcessBundle::class => ['all' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
