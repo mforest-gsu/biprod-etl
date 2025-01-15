d4add7d84f00998c9da34d2f6733c0fe1c221282
Melody Kimball <mforest@gsu.edu>
2025-01-15 12:09:25 -0500

    OneUSG ETL

---
+++ src/Command/HRM.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gsu\Biprod\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\InputInterface;
+
+#[AsCommand('biprod:hrm')]
+final class SIS extends Biprod
+{
+    /** @inheritdoc */
+    protected function getShellArgs(InputInterface $input): array
+    {
+        return array_map($this->shellCmd->sqlplus(...), [
+           'HRM_PERSON.sql'
+        ]);
+    }
+}
