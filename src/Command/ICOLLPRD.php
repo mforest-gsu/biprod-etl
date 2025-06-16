a8ca7aac166953af4abde5c3c12943b4495a1c6c
Melody Kimball <mforest@gsu.edu>
2025-06-16 15:11:29 -0400

    Moved ICOLLPRD sql into its own command

--- src/Command/ICOLLPRD.php
+++ src/Command/ICOLLPRD.php
@@ -7,15 +7,15 @@ namespace Gsu\Biprod\Command;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputInterface;
 
-#[AsCommand('biprod:lms')]
-final class LMS extends Biprod
+#[AsCommand('biprod:icollprd')]
+final class ICOLLPRD extends Biprod
 {
     /** @inheritdoc */
     protected function getShellArgs(InputInterface $input): array
     {
         return array_map($this->shellCmd->sqlplus(...), [
-            'LMSENRL.sql',
-            'LMS_ICOLLEGE_RUBRIC_PROG.sql'
+            'ICOLLPRD_LMSENRL.sql',
+            'ICOLLPRD_LMS_ICOLLEGE_RUBRIC_PROG.sql',
         ]);
     }
 }
